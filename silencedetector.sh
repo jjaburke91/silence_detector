4184cf752a1ccf6ff98a21a5b007f62faf910552
Jamie Burke <jjaburke@hotmail.com>
2018-09-13 12:03:42 +0100

    Minor tidy to code.

--- silencedetector.sh
+++ silencedetector.sh
@@ -1,33 +1,25 @@
 #!/bin/bash
+
 LIVE_AUDIO_STREAM="https://stream-relay-geo.ntslive.net/stream?t=1535983749542"
 SAMPLE_AUDIO="resources/sample_with_silence.mp3"
-OUT_AUDIO_FILENAME="out_audio.mp3"
 
-audio_source = LIVE_AUDIO_STREAM
-output_filename = OUT_AUDIO_FILENAME
+audio_source=$LIVE_AUDIO_STREAM
 while test $# -gt 0; do
-        case "$1" in
-             -file)
-                 shift
-                 audio_source=$1
-                 shift
-                 ;;
-            -output)
-                shift
-                output_filename=$1
-                shift
-                ;;
-             *)
-                echo "$1 is not a recognized flag!"
-                return 1;
-                ;;
-       esac
+    case "$1" in
+        -f)
+            shift
+            audio_source=$1
+            shift
+            ;;
+        *)
+            echo "$1 is not a recognized flag!"
+            return 1;
+            ;;
+    esac
 done
 
-echo $audio_source
-
-ffmpeg -y -i $audio_source -t 00:00:15 $output_filename >logs/stream_recording
+echo "$audio_source"
 
 # 2>&1 Pipes ffmpeg's sterr to stdout.
-# ffmpeg -i $OUT_AUDIO_FILENAME -af silencedetect=noise=0.0001 -f null - | grep 'silencedetect'
-# ffmpeg -i $SOURCE_PATH -af silencedetect=noise=0.0001 -f null - 2>&1 | grep 'silencedetect'
+ffmpeg -i $audio_source -af silencedetect=noise=0.0001 -f null -
+#ffmpeg -i $SOURCE_PATH -af silencedetect=noise=0.0001 -f null - 2>&1 | grep 'silencedetect'
